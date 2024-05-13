d81641cc95a02c0e5e814c1df4c597aea290f3b8
fabyDiaz <fbl.diaz.n@gmail.com>
2024-05-13 19:19:46 -0400

    se agrega la opcion de enviar por email y whatsapp

--- app/src/main/java/com/example/listapeliculas/MainActivity.kt
+++ app/src/main/java/com/example/listapeliculas/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.listapeliculas
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +13,8 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.listapeliculas.Entities.Pelicula
 import com.example.listapeliculas.adapters.PeliculaAdpater
 import com.example.listapeliculas.databinding.ActivityMainBinding
+import java.io.File
+import java.net.URLEncoder
 
 class MainActivity : AppCompatActivity() {
 
@@ -35,11 +40,11 @@ class MainActivity : AppCompatActivity() {
         binding.recyclerPelicula.adapter = peliculaAdapter
         peliculaAdapter.peliculas = Pelicula.dataPelicula
 
-       /* sismoAdapter.onItemClickListener = {sismo ->
-            //Toast.makeText(this,sismo.lugar, Toast.LENGTH_SHORT).show()
-            enviarCorreoElectronicoSismo(sismo)
-            enviarMensajeWhatsapp(sismo)
-        }*/
+       peliculaAdapter.onItemClickListener = {pelicula ->
+            Toast.makeText(this,"Esto es una pelicula", Toast.LENGTH_SHORT).show()
+            enviarCorreoElectronicoPelicula(pelicula)
+            enviarMensajeWhatsapp(pelicula)
+        }
 
         //peliculaAdapter.peliculas = Pelicula.dataEmpty
         if(peliculaAdapter.peliculas.isEmpty())
@@ -47,4 +52,45 @@ class MainActivity : AppCompatActivity() {
         else
             binding.empty.visibility = View.GONE
     }
+
+
+    private fun enviarCorreoElectronicoPelicula(pelicula: Pelicula){
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra(Intent.EXTRA_EMAIL, arrayOf("fabiola.dinar@gmail.com"))
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Envío información de película")
+        intent.putExtra(Intent.EXTRA_TEXT, "Título: ${pelicula.titulo}\nAño: ${pelicula.anio}\nDuración: ${pelicula.duracion}\nGénero: ${pelicula.genero}")
+
+        // Obtener la URI de la imagen
+        val uri = Uri.parse(pelicula.URLimagen)
+        // Adjuntar la imagen al intent
+        intent.putExtra(Intent.EXTRA_STREAM, uri)
+
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(Intent.createChooser(intent, "Enviar correo películas"))
+        } else {
+            Toast.makeText(this, "Debes tener una aplicación de correo electrónico", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun enviarMensajeWhatsapp(pelicula: Pelicula){
+
+       // val telefonoWsp = "+5492616101861"
+        val mensaje = "*Título:* ${pelicula.titulo}\n" +
+                "*Año:* ${pelicula.anio}\n" +
+                "*Duración:* ${pelicula.duracion} min\n" +
+                "*Género:* ${pelicula.genero}\n" +
+                "*URL de la imagen:* ${pelicula.URLimagen}"
+        val mensajeCodificado = URLEncoder.encode(mensaje, "UTF-8")
+        val uri = Uri.parse("https://api.whatsapp.com/send?&text=$mensajeCodificado")
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(Intent.createChooser(intent, "Enviar Película"))
+        } else {
+            Toast.makeText(this, "Debes tener WhatsApp instalado", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
 }
